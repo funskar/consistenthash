7407bc24cf6e8528c09b78619db44deb146955f5
Sanskar <sanskarjhanwar@gmail.com>
2019-05-22 21:29:17 +0530

    Add Get & AddNode methods, basic sanity test cases

--- consistent.go
+++ consistent.go
@@ -1,7 +1,10 @@
 package consistenthash
 
 import (
+	"fmt"
 	"hash/crc32"
+	"sort"
+	"strconv"
 	"sync"
 )
 
@@ -22,28 +25,72 @@ type HashRing struct {
 	hash             Hash
 	sortedNodeHashes uints
 	nodeHashMap      map[uint32]string
+	members          map[string]bool
 	sync.RWMutex
 }
 
-func (hr *HashRing) IsEmpty() bool {
-	return len(hr.sortedNodeHashes) == 0
+func (h *HashRing) IsEmpty() bool {
+	return len(h.sortedNodeHashes) == 0
 }
 
 func NewRing(fn Hash) *HashRing {
-	hr := &HashRing{
+	h := &HashRing{
 		hash:        fn,
 		nodeHashMap: make(map[uint32]string),
+		members:     make(map[string]bool),
 	}
-	if hr.hash == nil {
-		hr.hash = crc32.ChecksumIEEE
+	if h.hash == nil {
+		h.hash = crc32.ChecksumIEEE
 	}
-	return hr
+	return h
 }
 
-func (h *HashRing) AddNode(node string) {
-
+func (h *HashRing) AddNode(node string, weight int) error {
+	h.RWMutex.Lock()
+	defer h.RWMutex.Unlock()
+	if _, exists := h.members[node]; exists {
+		return fmt.Errorf("node with name %s already exists", node)
+	}
+	for i := 0; i < weight; i++ {
+		hashKey := h.hashKey(i, node)
+		h.sortedNodeHashes = append(h.sortedNodeHashes, hashKey)
+		h.nodeHashMap[hashKey] = node
+	}
+	sort.Sort(h.sortedNodeHashes)
+	return nil
 }
 
-func (h *HashRing) Get(key string) string {
-	return ""
+func (h *HashRing) hashKey(i int, node string) uint32 {
+	strKey := []byte(strconv.Itoa(i) + node)
+	return h.hash(strKey)
+}
+
+func (h *HashRing) Get(key string) (string, error) {
+	h.RLock()
+	defer h.RUnlock()
+	reqHash := h.hash([]byte(key))
+	nodeHash := h.search(reqHash)
+	return h.nodeHashMap[nodeHash], nil
+}
+
+func (h *HashRing) DeleteNode(node string) error {
+	if _, exists := h.members[node]; !exists {
+		return fmt.Errorf("node with name %s not found", node)
+	}
+	//TODO: code for node deletion
+	return nil
+}
+
+func (h *HashRing) search(reqHash uint32) uint32 {
+	//Binary search for nearest node after the hash of current request
+	fn := func(mid int) bool {
+		return h.sortedNodeHashes[mid] >= reqHash
+	}
+	hashKey := sort.Search(len(h.sortedNodeHashes), fn)
+
+	// Means we have cycled back to the first replica
+	if hashKey == len(h.sortedNodeHashes) {
+		hashKey = 0
+	}
+	return h.sortedNodeHashes[hashKey]
 }
--- consistent_test.go
+++ consistent_test.go
@@ -1,1 +1,88 @@
 package consistenthash
+
+import (
+	"hash/crc32"
+	"strconv"
+	"testing"
+)
+
+func checkCount(num, expected int, t *testing.T) {
+	if num != expected {
+		t.Errorf("got %d, expected %d", num, expected)
+	}
+}
+
+// Simple hash function to return easier to understand values
+// Assumes the keys can be converted to integer
+var hashFunction = func(data []byte) uint32 {
+	hash, err := strconv.Atoi(string(data))
+	if err != nil {
+		panic(err)
+	}
+	return uint32(hash)
+}
+
+func TestNewRing(t *testing.T) {
+	h := NewRing(crc32.ChecksumIEEE)
+	if h == nil {
+		t.Errorf("expected obj")
+	}
+}
+
+// Given the above simple hashFunction,
+// it will create virtual nodes - {1 4 7 11 14 17 21 24 27}
+var nodes = []struct {
+	Key    string
+	Weight int
+}{
+	{"1", 3},
+	{"4", 3},
+	{"7", 3},
+}
+
+var requests = []struct {
+	Key     string
+	expNode string
+}{
+	{"3", "4"},
+	{"0", "1"},
+	{"15", "7"},
+	{"6", "7"},
+}
+
+func TestHashRing_AddNode(t *testing.T) {
+	h := NewRing(hashFunction)
+	count := 0
+	for _, node := range nodes {
+		h.AddNode(node.Key, node.Weight)
+		count += node.Weight
+	}
+	checkCount(len(h.sortedNodeHashes), count, t)
+}
+
+func TestHashRing_IsEmpty(t *testing.T) {
+	h := NewRing(hashFunction)
+	if !h.IsEmpty() {
+		t.Error("Hash ring found to be non-empty")
+	}
+	h.AddNode("1", 5)
+	if h.IsEmpty() {
+		t.Error("Hash ring found to be empty")
+	}
+}
+
+func TestHashRing_Get(t *testing.T) {
+	h := NewRing(hashFunction)
+	for _, node := range nodes {
+		h.AddNode(node.Key, node.Weight)
+	}
+	for _, req := range requests {
+		node, err := h.Get(req.Key)
+		if err != nil {
+			t.Errorf("Error while trying to fetch Node for Request - %s", req.Key)
+		}
+		if node != req.expNode {
+			t.Errorf("Request for %s, yielded node %s, but expected node %s", req.Key, node, req.expNode)
+		}
+	}
+}
